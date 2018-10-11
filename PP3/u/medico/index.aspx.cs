1daf5a90d26518b57f25cbaa039562f2c3693015
jae hnri <joao.henri.cr@gmail.com>
2018-10-11 11:27:11 -0300

    Medico
    Começo da parte do Médico

---
+++ PP3/u/medico/index.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PP3.u.medico
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_Agenda_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("agenda.aspx");
+        }
+
+        protected void btn_Consulta_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("consulta.aspx");
+        }
+
+        protected void btn_Graficos_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("graficos.aspx");
+        }
+    }
+}
